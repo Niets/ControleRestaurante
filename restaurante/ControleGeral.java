9f1e5658c5f2dfbc87d9ddb862c9fe23a9a8f71a
Nicolas Boarin <nicolas.boarin@gmail.com>
2014-07-06 16:16:43 -0300

    Criação da classe main que ativa o ControleEscolha

---
+++ restaurante/ControleGeral.java
@@ -0,0 +1,10 @@
+package restaurante;
+
+public class ControleGeral {
+    
+    public static void main(String[] args) {
+        ControleEscolha cadastro = new ControleEscolha();
+        cadastro.setVisible(true);
+    }
+    
+}
